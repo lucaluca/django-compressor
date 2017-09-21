b8adde15f7462154e3a1a0d241992971de091028
Jonathan Giuffrida <jcgiuffri@gmail.com>
2017-09-20 22:53:34 -0500

    I don't understand python format

--- compressor/filters/uglifyjs.py
+++ compressor/filters/uglifyjs.py
@@ -3,10 +3,10 @@ from compressor.filters import CompilerFilter
 
 
 class UglifyJSFilter(CompilerFilter):
-    command = "{binary} {args}".format({
-        'binary': settings.COMPRESS_UGLIFY_JS_BINARY,
-        'args': settings.COMPRESS_UGLIFY_JS_ARGUMENTS
-    })
+    command = "{binary} {args}".format(
+        binary=settings.COMPRESS_UGLIFY_JS_BINARY,
+        args=settings.COMPRESS_UGLIFY_JS_ARGUMENTS
+    )
 
     def __init__(self, *args, **kwargs):
         super(UglifyFilter, self).__init__(*args, **kwargs)
