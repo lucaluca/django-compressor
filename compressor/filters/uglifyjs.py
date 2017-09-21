ae573862fed5f9c1049f3195b09c8f5e593e42b8
Jonathan Giuffrida <jcgiuffri@gmail.com>
2017-09-20 22:49:17 -0500

    Add pure Uglify JS filter

---
+++ compressor/filters/uglifyjs.py
@@ -0,0 +1,13 @@
+from compressor.conf import settings
+from compressor.filters import CompilerFilter
+
+
+class UglifyJSFilter(CompilerFilter):
+    command = "{binary} {args}".format({
+        'binary': settings.COMPRESS_UGLIFY_JS_BINARY,
+        'args': settings.COMPRESS_UGLIFY_JS_ARGUMENTS
+    })
+
+    def __init__(self, *args, **kwargs):
+        super(UglifyFilter, self).__init__(*args, **kwargs)
+        self.command
